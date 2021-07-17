0d87effba4475aba65498ed03c37335edecf14e1
Eiger <eiger.ai@qq.com>
2021-07-17 18:13:43 +0800

    update:保存一下

---
+++ src/snippets/chap3/hello.go
@@ -0,0 +1,11 @@
+// package main main表示包名
+package main
+
+// 导入"fmt"这个包
+import "fmt"
+
+// 程序的入口 main函数
+func main() {
+	// 调用"fmt"包下的Println()函数向标准输出打印"hello world!"字符串
+	fmt.Println("hello, world!")
+}
